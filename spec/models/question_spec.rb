92796a1bca9e47101c172831a1200b0939482f9c
Anton Lipich <anton.lipich1@gmail.com>
2021-03-10 14:29:15 +0300

    add text uniqueness validate test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -11,9 +11,11 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
 
   context 'validations check' do
+    subject { Question.new(text: 'Два плюс два', level: 0, answer1: '2', answer2: '3', answer3: '4', answer4: '9') }
 
     it { should validate_presence_of :text }
     it { should validate_presence_of :level }
+    it { should validate_uniqueness_of :text }
 
     it { should validate_inclusion_of(:level).in_range(0..14) }
 
