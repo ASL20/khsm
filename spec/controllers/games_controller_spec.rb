56984762228e88f67333fe8a78232f9353691c3b
Anton Lipich <anton.lipich1@gmail.com>
2021-04-01 17:16:26 +0300

    add fifty_fifty test in games_controller

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -164,7 +164,7 @@ RSpec.describe GamesController, type: :controller do
     it 'uses audience help' do
       # сперва проверяем что в подсказках текущего вопроса пусто
       expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
-      expect(game_w_questions.audience_help_used).to be_falsey
+      expect(game_w_questions.audience_help_used).to be false
 
       # фигачим запрос в контроллен с нужным типом
       put :help, id: game_w_questions.id, help_type: :audience_help
@@ -175,6 +175,24 @@ RSpec.describe GamesController, type: :controller do
       expect(game.audience_help_used).to be_truthy
       expect(game.current_game_question.help_hash[:audience_help]).to be
       expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+      expect(response).to redirect_to(game_path(game))
+    end
+
+    it 'uses fifty_fifty' do
+      # сперва проверяем что в подсказках текущего вопроса пусто
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be false
+
+      # фигачим запрос в контроллен с нужным типом
+      put :help, id: game_w_questions.id, help_type: :fifty_fifty
+      game = assigns(:game)
+
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be false
+      expect(game.fifty_fifty_used).to be true
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to be
+      expect(game.current_game_question.help_hash[:fifty_fifty]).to include('d')
+      expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq(2)
       expect(response).to redirect_to(game_path(game))
     end
   end
